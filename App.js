02cbf6b82e589d6fa11ef589ab205b1b75a8b853
niksync1 <40718182+niksync1@users.noreply.github.com>
2023-04-04 23:09:38 +0000

    global stylesheet

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState} from 'react';
+import Home from './screens/home';
+import * as Font from './screens/home';
+import {AppLoading } from 'expo';
+
+const getFonts = () => Font.loadAsync({
+    'nunito-regular': require('./assets/fonts/NunitoSans-Regular.ttf'),
+    'nunito-bold' : require('./assets/fonts/NunitoSans-Bold.ttf')
+  });
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if(fontsLoaded){
+    return (
+      <Home />
+    );
+  }else { 
+    return(
+      <AppLoading
+      startAsync={getFonts}
+      onFinish={()=> setFontsLoaded(true)}
+    />
+    )
+  }  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
